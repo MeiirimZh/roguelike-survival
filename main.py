8059d9db8a069c67745db0fab6329f569d70b19f
MeiirimZh <zh.meyirim@gmail.com>
2025-01-27 16:58:09 +0500

    added states

--- main.py
+++ main.py
@@ -7,8 +7,14 @@ from config import SCREENWIDTH, SCREENHEIGHT, FPS
 class Game:
     def __init__(self):
         pygame.init()
-        self.screen = pygame.display.set_mode((SCREENWIDTH, SCREENHEIGHT))
+        self.screen = pygame.display.set_mode((SCREENWIDTH, SCREENHEIGHT), pygame.FULLSCREEN)
         self.clock = pygame.time.Clock()
+
+        self.game_state_manager = GameStateManager('World')
+        self.main_menu = MainMenu(self.screen, self.game_state_manager)
+        self.world = World(self.screen, self.game_state_manager)
+
+        self.states = {'Main Menu': self.main_menu, 'World': self.world}
 
     def run(self):
         while True:
@@ -17,8 +23,39 @@ class Game:
                     pygame.quit()
                     sys.exit()
 
+            self.states[self.game_state_manager.get_state()].run()
+
             pygame.display.update()
             self.clock.tick(FPS)
+
+
+class World:
+    def __init__(self, display, game_state_manager):
+        self.display = display
+        self.game_state_manager = game_state_manager
+
+    def run(self):
+        self.display.fill('blue')
+
+
+class MainMenu:
+    def __init__(self, display, game_state_manager):
+        self.display = display
+        self.game_state_manager = game_state_manager
+
+    def run(self):
+        self.display.fill('red')
+
+
+class GameStateManager:
+    def __init__(self, current_state):
+        self.current_state = current_state
+
+    def get_state(self):
+        return self.current_state
+
+    def set_state(self, state):
+        self.current_state = state
 
 
 if __name__ == '__main__':
